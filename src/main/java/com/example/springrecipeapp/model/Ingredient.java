7ad54308134d7847dd23916949f51dc8d79ef4cc
Durran0490 <aleksands_riga@yahoo.com>
2020-07-31 13:07:02 +0300

    Refactored. Project Lombok added

--- src/main/java/com/example/springrecipeapp/model/Ingredient.java
+++ src/main/java/com/example/springrecipeapp/model/Ingredient.java
@@ -1,9 +1,11 @@
 package com.example.springrecipeapp.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Set;
-
+@Data
 @Entity
 public class Ingredient {
 
@@ -35,44 +37,4 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }

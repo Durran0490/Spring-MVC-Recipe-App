ac719e2c6dc087cc9a2ea54c38c363a4292e2e4e
Durran0490 <aleksands_riga@yahoo.com>
2020-08-07 18:27:26 +0300

    I'm completely lost at this point (added new Ingredient implementation)

--- src/main/java/com/example/springrecipeapp/controllers/IngredientController.java
+++ src/main/java/com/example/springrecipeapp/controllers/IngredientController.java
@@ -3,16 +3,15 @@ package com.example.springrecipeapp.controllers;
 import com.example.springrecipeapp.commands.IngredientCommand;
 import com.example.springrecipeapp.commands.RecipeCommand;
 import com.example.springrecipeapp.commands.UnitOfMeasureCommand;
+import com.example.springrecipeapp.model.Ingredient;
+import com.example.springrecipeapp.model.Recipe;
 import com.example.springrecipeapp.services.IngredientService;
 import com.example.springrecipeapp.services.RecipeService;
 import com.example.springrecipeapp.services.UnitOfMeasureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -22,7 +21,7 @@ public class IngredientController {
     private final IngredientService ingredientService;
     private final UnitOfMeasureService unitOfMeasureService;
 
-    public IngredientController(RecipeService recipeService,IngredientService ingredientService, UnitOfMeasureService unitOfMeasureService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService, UnitOfMeasureService unitOfMeasureService) {
         this.recipeService = recipeService;
         this.ingredientService = ingredientService;
         this.unitOfMeasureService = unitOfMeasureService;
@@ -77,5 +76,19 @@ public class IngredientController {
         return "redirect:/recipe/"+ recipeId +"/ingredients";
     }
 
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newRecipe(@PathVariable String recipeId, Model model){
+        //
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
 
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("oumList", unitOfMeasureService.listAllUoms());
+        return "recipe/ingredient/ingredientform";
+    }
 }
